4f0ae8d469ceac4c5ddd342a163ba36817370d1b
ZoirJohn <zoirbekzokirjonov2007@gmail.com>
2025-07-29 20:54:03 +0500

    refactor: standardize import formatting and clean up EventCard and scheduleFormSchema

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -1,25 +1,20 @@
-import { TEvent } from "@/app/api/events/[userId]/route";
-import { Card, CardContent, CardDescription, CardTitle } from "./ui/card";
-import Buttons from "./Buttons";
+import { TEvent } from '@/app/api/events/[userId]/route';
+import { Card, CardContent, CardDescription, CardTitle } from './ui/card';
+import Buttons from './Buttons';
 
-export default async function EventCard({
-      id,
-      description,
-      name,
-      durationInMinutes,
-}: TEvent) {
-      return (
-            <Card className="px-6 gap-1 w-full">
-                  <CardTitle>
-                        <h1 className="text-2xl">{name}</h1>
-                  </CardTitle>
-                  <CardDescription>
-                        <p>{durationInMinutes} minutes</p>
-                  </CardDescription>
-                  <CardContent className="flex flex-col gap-2 px-0">
-                        <p className="my-4 text-xl">{description}</p>
-                        <Buttons eventId={id} />
-                  </CardContent>
-            </Card>
-      );
+export default async function EventCard({ id, description, name, durationInMinutes }: TEvent) {
+        return (
+                <Card className="px-6 gap-1 w-full">
+                        <CardTitle>
+                                <h1 className="text-2xl">{name}</h1>
+                        </CardTitle>
+                        <CardDescription>
+                                <p>{durationInMinutes} minutes</p>
+                        </CardDescription>
+                        <CardContent className="flex flex-col gap-2 px-0">
+                                <p className="my-4 text-xl">{description}</p>
+                                <Buttons eventId={id} />
+                        </CardContent>
+                </Card>
+        );
 }
--- schema/schedule.ts
+++ schema/schedule.ts
@@ -1,41 +1,37 @@
-import {DAYS_OF_WEEK_IN_ORDER} from "@/constants";
-import { timeToFloat } from "@/lib/utils";
-import z from "zod";
+import { DAYS_OF_WEEK_IN_ORDER } from '@/constants';
+import { timeToFloat } from '@/lib/utils';
+import z from 'zod';
 
 export const scheduleFormSchema = z.object({
-	timezone: z.string().min(1, "Required"),
-	availabilities: z
-		.array(
-			z.object({
-				startTime: z
-					.string()
-					.regex(/^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/, "Time must be in HH:MM format"),
-				endTime: z
-					.string()
-					.regex(/^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/, "Time must be in HH:MM format"),
-				dayOfWeek: z.enum(DAYS_OF_WEEK_IN_ORDER),
-			})
-		)
-		.refine((availabilities) => {
-			for (let index = 0; index < availabilities.length; index++) {
-				const availability = availabilities[index];
+        timezone: z.string().min(1, 'Required'),
+        availabilities: z
+                .array(
+                        z.object({
+                                startTime: z.string().regex(/^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/, 'Time must be in HH:MM format'),
+                                endTime: z.string().regex(/^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/, 'Time must be in HH:MM format'),
+                                dayOfWeek: z.enum(DAYS_OF_WEEK_IN_ORDER),
+                        })
+                )
+                .refine((availabilities) => {
+                        for (let index = 0; index < availabilities.length; index++) {
+                                const availability = availabilities[index];
 
-				const overlaps = availabilities.some((a, i) => {
-					return (
-						i !== index &&
-						a.dayOfWeek === availability.dayOfWeek &&
-						timeToFloat(a.startTime) < timeToFloat(availability.endTime) &&
-						timeToFloat(a.endTime) > timeToFloat(availability.startTime)
-					);
-				});
-				if (overlaps) {
-					return false;
-				}
+                                const overlaps = availabilities.some((a, i) => {
+                                        return (
+                                                i !== index &&
+                                                a.dayOfWeek === availability.dayOfWeek &&
+                                                timeToFloat(a.startTime) < timeToFloat(availability.endTime) &&
+                                                timeToFloat(a.endTime) > timeToFloat(availability.startTime)
+                                        );
+                                });
+                                if (overlaps) {
+                                        return false;
+                                }
 
-				if (timeToFloat(availability.startTime) >= timeToFloat(availability.endTime)) {
-					return false;
-				}
-			}
-			return true;
-		}, "Invalid availability times or overlapping slots"),
+                                if (timeToFloat(availability.startTime) >= timeToFloat(availability.endTime)) {
+                                        return false;
+                                }
+                        }
+                        return true;
+                }, 'Invalid availability times or overlapping slots'),
 });
